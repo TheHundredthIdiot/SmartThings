bc9b216751023b6f8ac18e9773d021d0267d8095
Andy <andycleveland00@gmail.com>
2016-02-28 09:46:00 +0000

    Update NetatmoBasestation.groovy

--- Netatmo/NetatmoBasestation.groovy
+++ Netatmo/NetatmoBasestation.groovy
@@ -1,4 +1,4 @@
-/*
+	/*
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
@@ -19,8 +19,11 @@
 
 metadata {
 	definition (name: "My Netatmo Basestation", namespace: "TheHundredthIdiot", author: "Andy") {
+    
+    	capability "Sensor"
 		capability "Relative Humidity Measurement"
 		capability "Temperature Measurement"
+//      capability "Sound Pressure Level"
 
 		attribute "carbonDioxide", "string"
 		attribute "noise", "string"
